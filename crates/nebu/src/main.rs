188de3800fec68936ce1bbc11a86fa0bedb73f7c
Kasper Jønsson <dkkasjoe@hotmail.com>
2025-07-03 14:41:32 +0200

    Tokio & small typos

--- crates/nebu/src/main.rs
+++ crates/nebu/src/main.rs
@@ -19,7 +19,7 @@ mod cmds;
 )]
 struct Cli {
     #[command(subcommand)]
-    command: Option<Commands>,
+    command: Commands,
 
     /// Global arguments for all commands
     #[command(flatten)]
@@ -48,9 +48,11 @@ enum Commands {
 }
 
 #[derive(clap::Parser, Debug)]
+#[command(next_help_heading = "Global options", next_display_order = 1000)]
 struct GlobalArgs {
     #[arg(
         global = true,
+        help = "Output format for the command results.",
         short = 'F',
         long,
         env = "NEBU_OUTPUT_FORMAT",
@@ -60,6 +62,7 @@ struct GlobalArgs {
 
     #[arg(
         global = true,
+        help = "Path to the configuration file for nebu.",
         long,
         env = "NEBU_CONFIG_PATH",
         default_value = "~/.config/nebu"
@@ -68,6 +71,7 @@ struct GlobalArgs {
 
     #[arg(
         global = true,
+        help = "Path to the home directory for nebu.",
         hide = true,
         long,
         env = "NEBU_HOME_PATH",
@@ -77,6 +81,7 @@ struct GlobalArgs {
 
     #[arg(
         global = true,
+        help = "Enable verbose output. Use multiple times for more verbosity.",
         short = 'v',
         env = "NEBU_VERBOSE",
         action = clap::ArgAction::Count,
@@ -104,28 +109,27 @@ impl From<OutputFormats> for clap::builder::OsStr {
     }
 }
 
-fn main() -> ExitCode {
+#[tokio::main]
+async fn main() -> ExitCode {
     let cli = Cli::parse();
 
     tracing_subscriber::fmt()
         .without_time()
-        .with_target(false)
         .with_env_filter(EnvFilter::try_from_default_env().unwrap_or_else(|_| {
             match cli.global_args.verbose {
                 0 => EnvFilter::from("none"),
-                1 => EnvFilter::from("nebu=info"),
-                2 => EnvFilter::from("nebu=debug"),
-                _ => EnvFilter::from("nebu=trace"),
+                1 => EnvFilter::from("none,nebu=info"),
+                2 => EnvFilter::from("none,nebu=debug"),
+                _ => EnvFilter::from("none,nebu=trace"),
             }
         }))
         .init();
 
     match &cli.command {
-        Some(Commands::Version) => cmds::version::nebu_version(cli.global_args),
-        Some(Commands::Env(_env)) => todo!(),
-        Some(Commands::Project(_project)) => todo!(),
-        Some(Commands::Infra) => todo!(),
-        None => todo!(), // Make a decision on what the default command should be.
+        Commands::Version => cmds::version::nebu_version(cli.global_args),
+        Commands::Env(_env) => todo!(),
+        Commands::Project(_project) => todo!(),
+        Commands::Infra => todo!(),
     };
 
     ExitCode::SUCCESS
