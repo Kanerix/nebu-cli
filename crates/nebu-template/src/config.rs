3e83a4b07a4b16d3cd3ec054c67a7c32b97f6bdc
Kasper Jønsson <dkkasjoe@hotmail.com>
2025-07-18 14:49:46 +0200

    Added tempalte structs

--- crates/nebu-template/src/config.rs
+++ crates/nebu-template/src/config.rs
@@ -2,28 +2,56 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Config {
-    questions: Vec<String>,
+    pub questions: Vec<String>,
+    pub actions: Vec<QuestionAction>,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct Component {
+    pub id: String,
+    pub description: String,
+    pub aliases: Vec<String>,
+    pub target: ComponentTarget,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "lowercase")]
+#[serde(tag = "kind")]
+pub enum ComponentTarget {
+    Folder { path: String },
+    File { path: String },
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Question {
-    prompt: String,
-    kind: QuestionKind,
+    pub prompt: String,
+    pub kind: QuestionKind,
+    pub subquestions: Vec<Question>,
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "lowercase")]
+#[serde(tag = "kind")]
 pub enum QuestionKind {
-    #[serde(rename = "boolean")]
-    Bool(bool),
+    #[serde(rename = "bool")]
+    Bool { default: bool, component: String },
     #[serde(rename = "input")]
-    Input(String)
+    Input { default: String, component: String },
 }
 
-impl Question {
-    pub fn new(prompt: String, kind: QuestionKind) -> Self {
-        Question {
-            prompt,
-            kind,
-        }
-    }
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "lowercase")]
+pub enum QuestionAction {
+    /// Replace content from files within the given glob pattern.
+    #[serde(rename = "replace_content")]
+    ReplaceContent { glob: String },
+    /// Include the folder from the given path.
+    #[serde(rename = "include_folder")]
+    IncludeFolder { glob: String },
+    /// Rename a folder.
+    #[serde(rename = "rename_folder")]
+    RenameFolder { glob: String, name: String },
+    /// Rename a file.
+    #[serde(rename = "rename_file")]
+    RenameFile { glob: String, name: String },
 }
